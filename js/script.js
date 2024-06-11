fd801481c0c8e039ea2aefbfcf3d6105048cec02
Jose Rodrigo Cari Almiron <jcarial@unsa.edu.pe>
2024-06-11 00:25:36 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (name === '' || email === '' || message === '') {
+        alert('Por favor complete todos los campos.');
+        event.preventDefault();
+    } else {
+        alert('Formulario enviado con éxito.');
+    }
+});
